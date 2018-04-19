19be9c540188c98ec43861658cd0ea0852b56c9f
Ashleigh Maund <ashleighmaund@gmail.com>
2018-04-19 09:51:49 +0100

    adding mock heroes list to show

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: "Dr Dweeb"},
+  {id: 12, name: "The Smellinator"},
+  {id: 13, name: "Buttman"},
+  {id: 14, name: "Dog Person"},
+  {id: 15, name: "The Budgie Whisperer"},
+  {id: 16, name: "Little Miss Muffet"},
+  {id: 17, name: "A Bowl of Muesli"},
+  {id: 18, name: "Seven Thumbs on One Hand"},
+  {id: 19, name: "Spiderflan"},
+  {id: 20, name: "Captain Canada"}
+];
